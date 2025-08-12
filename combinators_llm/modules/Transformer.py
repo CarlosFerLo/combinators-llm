6f8fff55f2caa94b9cc8e58e1d8098f52fab0114
Carlos Fernandez <carlos.ferlo@outlook.com>
2025-08-12 17:35:46 +0200

    build transformer model

---
+++ combinators_llm/modules/Transformer.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+from Encoder import Encoder
+from Decoder import Decoder
+from InputEmbedding import InputEmbedding
+from PositionalEncoding import PositionalEncoding
+from ProjectionLayer import ProjectionLayer
+
+class Transformer (nn.Module) :
+    
+    def __init__ (self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbedding, tgt_embed: InputEmbedding, src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None :
+        super().__init__() 
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+        
+    def encode (self, src, src_mask) :
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+    
+    def decode (self, encoder_output, src_mask, tgt, tgt_mask) :
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+    
+    def project (self, x) :
+        return self.projection_layer(x)
+        
---
+++ combinators_llm/modules/__init__.py
@@ -0,0 +1,27 @@
+from InputEmbedding import InputEmbedding
+from PositionalEncoding import PositionalEncoding
+from LayerNormalization import LayerNormalization
+from FeedForwardBlock import FeedForwardBlock
+from MultiHeadAttentionBlock import MultiHeadAttentionBlock
+from ResidualConnection import ResidualConnection
+from EncoderBlock import EncoderBlock
+from Encoder import Encoder
+from DecoderBlock import DecoderBlock
+from Decoder import Decoder
+from ProjectionLayer import ProjectionLayer
+from Transformer import Transformer
+
+__all__ = [
+    "InputEmbedding",
+    "PositionalEncoding",
+    "LayerNormalization",
+    "FeedForwardBlock",
+    "MultiHeadAttentionBlock",
+    "ResidualConnection",
+    "EncoderBlock",
+    "Encoder",
+    "DecoderBlock",
+    "Decoder",
+    "ProjectionLayer",
+    "Transformer"
+]
