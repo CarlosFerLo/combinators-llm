bbe7d7957e84d0018663fb2269cef7944d72ab10
Carlos Fernandez <carlos.ferlo@outlook.com>
2025-08-13 19:36:51 +0200

    tokenizer for decoder

---
+++ combinators_llm/tokenizers/term.py
@@ -0,0 +1,30 @@
+from tokenizers import Tokenizer, Regex
+from tokenizers.models import WordLevel
+from tokenizers.pre_tokenizers import Split, Sequence
+
+# Allowed tokens
+vocab = {
+    "[UNK]": 0,
+    "[PAD]": 1,
+    "[SOS]": 2,
+    "[EOS]": 3,
+    "S": 4,
+    "K": 5,
+    "(": 6,
+    ")": 7
+}
+
+
+
+    
+
+
+if __name__ == "__main__" :
+    tokenizer = Tokenizer(WordLevel(vocab=vocab, unk_token="[UNK]"))
+    tokenizer.pre_tokenizer = Sequence([
+        Split(Regex("\s"), behavior="removed"),
+        Split("", behavior="removed")
+        ])
+    
+    
+    tokenizer.save("combinators_llm/tokenizers/term.json")
