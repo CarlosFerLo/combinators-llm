4c34891b1954e2755f1787be0e1ea9f769a10fa7
Carlos Fernandez <carlos.ferlo@outlook.com>
2025-08-27 18:29:18 +0200

    solve bug in get_tokenizer

--- combinators_llm/tokenizers/base.py
+++ combinators_llm/tokenizers/base.py
@@ -4,7 +4,7 @@ from pathlib import Path
 TOKENIZER_FOLDER = Path("combinators_llm/tokenizers")
 
 def get_tokenizer (name: str) -> Tokenizer :
-    tokenizer_path = TOKENIZER_FOLDER / f"{name}-tokenizer.json"
+    tokenizer_path = str(TOKENIZER_FOLDER / f"{name}-tokenizer.json")
     tokenizer = Tokenizer.from_file(tokenizer_path)
     
     return tokenizer
